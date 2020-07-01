1928330c8db3fa9f0a2fc31e6a54a16401eb638e
Natalia Yuzhakova <natalia.yuzhakova@gmail.com>
2020-07-01 03:25:29 +0000

    edited

--- timer1.js
+++ timer1.js
@@ -1,5 +1,3 @@
-  
-// ALARM CLOCK
 const data = process.argv.slice(2);
 let numbers = data.filter(number => {
   return number > 0;
@@ -15,3 +13,7 @@ const timer = function() {
 };
 
 timer(numbers);
+
+//Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
+//Example usage: node timer1.js 10 3 5 15 9 
+
